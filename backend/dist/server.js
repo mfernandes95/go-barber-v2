4442ee0cc7f185f037f571c0fa54873c7dbe13ba
mpinheiro <mpinheiro@orion.tech>
2020-07-18 20:54:27 -0300

    config TS/ESlint/Babel/Jest

---
+++ backend/dist/server.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+require("./controllers/UsersController");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express.default)();
+app.get('/', (request, response) => {
+  return response.json({
+    message: 'Hello World'
+  });
+});
+app.listen(3333);
